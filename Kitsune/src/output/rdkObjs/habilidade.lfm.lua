27dfe87eafd986def074ac356bffae82777a65bd
Rafael Ishikawa <rafkawa@gmail.com>
2019-09-02 01:56:30 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Kitsune/src/output/rdkObjs/habilidade.lfm.lua
@@ -0,0 +1,181 @@
+require("firecast.lua");
+local __o_rrpgObjs = require("rrpgObjs.lua");
+require("rrpgGUI.lua");
+require("rrpgDialogs.lua");
+require("rrpgLFM.lua");
+require("ndb.lua");
+require("locale.lua");
+local __o_Utils = require("utils.lua");
+
+local function constructNew_frmHabilidade()
+    local obj = GUI.fromHandle(_obj_newObject("form"));
+    local self = obj;
+    local sheet = nil;
+
+    rawset(obj, "_oldSetNodeObjectFunction", rawget(obj, "setNodeObject"));
+
+    function obj:setNodeObject(nodeObject)
+        sheet = nodeObject;
+        self.sheet = nodeObject;
+        self:_oldSetNodeObjectFunction(nodeObject);
+    end;
+
+    function obj:setNodeDatabase(nodeObject)
+        self:setNodeObject(nodeObject);
+    end;
+
+    _gui_assignInitialParentForForm(obj.handle);
+    obj:beginUpdate();
+    obj:setHeight(20);
+    obj:setMargins({top=2});
+    obj:setName("frmHabilidade");
+
+    obj.flowLayout1 = GUI.fromHandle(_obj_newObject("flowLayout"));
+    obj.flowLayout1:setParent(obj);
+    obj.flowLayout1:setAlign("client");
+    obj.flowLayout1:setHeight(20);
+    obj.flowLayout1:setWidth(300);
+    obj.flowLayout1:setName("flowLayout1");
+
+    obj.edit1 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit1:setParent(obj.flowLayout1);
+    obj.edit1:setField("nome");
+    obj.edit1:setHeight(20);
+    obj.edit1:setHorzTextAlign("center");
+    obj.edit1:setMargins({left=2});
+    obj.edit1:setWidth(200);
+    obj.edit1:setName("edit1");
+
+    obj.edit2 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit2:setParent(obj.flowLayout1);
+    obj.edit2:setField("formulaDado");
+    obj.edit2:setHeight(20);
+    obj.edit2:setHorzTextAlign("center");
+    obj.edit2:setMargins({left=2});
+    obj.edit2:setWidth(30);
+    obj.edit2:setName("edit2");
+
+    obj.label1 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label1:setParent(obj.flowLayout1);
+    obj.label1:setHeight(20);
+    obj.label1:setMargins({left=5});
+    obj.label1:setText("D100");
+    obj.label1:setWidth(40);
+    obj.label1:setName("label1");
+
+    obj.edit3 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit3:setParent(obj.flowLayout1);
+    obj.edit3:setField("formulaMod");
+    obj.edit3:setHeight(20);
+    obj.edit3:setHorzTextAlign("center");
+    obj.edit3:setMargins({left=2});
+    obj.edit3:setWidth(30);
+    obj.edit3:setName("edit3");
+
+    obj.edit4 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit4:setParent(obj.flowLayout1);
+    obj.edit4:setField("custo");
+    obj.edit4:setHeight(20);
+    obj.edit4:setHorzTextAlign("center");
+    obj.edit4:setMargins({left=2});
+    obj.edit4:setWidth(30);
+    obj.edit4:setName("edit4");
+
+    obj.button1 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button1:setParent(obj.flowLayout1);
+    obj.button1:setHeight(20);
+    obj.button1:setMargins({left=2});
+    obj.button1:setText("*");
+    obj.button1:setWidth(20);
+    obj.button1:setName("button1");
+
+    obj.button2 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button2:setParent(obj.flowLayout1);
+    obj.button2:setHeight(20);
+    obj.button2:setMargins({left=2});
+    obj.button2:setText("X");
+    obj.button2:setWidth(20);
+    obj.button2:setName("button2");
+
+    obj.dataLink1 = GUI.fromHandle(_obj_newObject("dataLink"));
+    obj.dataLink1:setParent(obj);
+    obj.dataLink1:setFields({'formulaMod'});
+    obj.dataLink1:setName("dataLink1");
+
+    obj._e_event0 = obj.button1:addEventListener("onClick",
+        function (_)
+            usarHabilidade(sheet)
+        end, obj);
+
+    obj._e_event1 = obj.button2:addEventListener("onClick",
+        function (_)
+            deleteHabilidade(sheet)
+        end, obj);
+
+    obj._e_event2 = obj.dataLink1:addEventListener("onChange",
+        function (_, field, oldValue, newValue)
+            updateSkill(sheet);
+        end, obj);
+
+    function obj:_releaseEvents()
+        __o_rrpgObjs.removeEventListenerById(self._e_event2);
+        __o_rrpgObjs.removeEventListenerById(self._e_event1);
+        __o_rrpgObjs.removeEventListenerById(self._e_event0);
+    end;
+
+    obj._oldLFMDestroy = obj.destroy;
+
+    function obj:destroy() 
+        self:_releaseEvents();
+
+        if (self.handle ~= 0) and (self.setNodeDatabase ~= nil) then
+          self:setNodeDatabase(nil);
+        end;
+
+        if self.edit3 ~= nil then self.edit3:destroy(); self.edit3 = nil; end;
+        if self.flowLayout1 ~= nil then self.flowLayout1:destroy(); self.flowLayout1 = nil; end;
+        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
+        if self.button1 ~= nil then self.button1:destroy(); self.button1 = nil; end;
+        if self.edit4 ~= nil then self.edit4:destroy(); self.edit4 = nil; end;
+        if self.label1 ~= nil then self.label1:destroy(); self.label1 = nil; end;
+        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.button2 ~= nil then self.button2:destroy(); self.button2 = nil; end;
+        if self.dataLink1 ~= nil then self.dataLink1:destroy(); self.dataLink1 = nil; end;
+        self:_oldLFMDestroy();
+    end;
+
+    obj:endUpdate();
+
+    return obj;
+end;
+
+function newfrmHabilidade()
+    local retObj = nil;
+    __o_rrpgObjs.beginObjectsLoading();
+
+    __o_Utils.tryFinally(
+      function()
+        retObj = constructNew_frmHabilidade();
+      end,
+      function()
+        __o_rrpgObjs.endObjectsLoading();
+      end);
+
+    assert(retObj ~= nil);
+    return retObj;
+end;
+
+local _frmHabilidade = {
+    newEditor = newfrmHabilidade, 
+    new = newfrmHabilidade, 
+    name = "frmHabilidade", 
+    dataType = "", 
+    formType = "undefined", 
+    formComponentName = "form", 
+    title = "", 
+    description=""};
+
+frmHabilidade = _frmHabilidade;
+Firecast.registrarForm(_frmHabilidade);
+
+return _frmHabilidade;
